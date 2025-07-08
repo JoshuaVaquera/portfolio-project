c3f97c354f34a700f320c289aa222cdc05786731
Joshua Vaquera <vaquerajosh@gmail.com>
2025-07-08 16:42:26 -0400

    changed it up a bit with buttons and functions

--- root-directory/html/code/node.js
+++ root-directory/html/code/node.js
@@ -1,3 +1,14 @@
 function test() {
     console.log("test");
 }
+
+test();
+
+function toggleImage(id) {
+    var imgDiv = document.getElementById(id);
+    if (imgDiv.style.display === "none") {
+        imgDiv.style.display = "block";
+    } else {
+        imgDiv.style.display = "none";
+    }
+}
